3acc3dcb30bb78ce609f83817477003ee9eeae2f
David Guibert <david.guibert@gmail.com>
2022-04-29 15:06:11 +0200

    flake.lock: Update
    Flake lock file changes:
    
    • Updated input 'nixpkgs':
        'github:dguibert/nixpkgs/9a2b7ba1adae9a95c9a4b8f719fbb615d06e2a54' (2022-04-24)
      → 'github:dguibert/nixpkgs/bc2ea7d294f0dc159251283eb5bee80c137fa9ca' (2022-04-27)

--- flake-prod/flake.nix
+++ flake-prod/flake.nix
@@ -2,14 +2,15 @@
   description = "A flake for building packages on /software-like structure";
 
   inputs = {
-    nixpkgs.url          = "github:dguibert/nixpkgs/pu-nixpack";
+    #nixpkgs.url          = "github:dguibert/nixpkgs/pu";
+    nixpkgs.url          = "github:dguibert/nixpkgs?rev=bc2ea7d294f0dc159251283eb5bee80c137fa9ca";
 
     nix.url              = "github:dguibert/nix/pu";
     nix.inputs.nixpkgs.follows = "nixpkgs";
 
     flake-utils.url = "github:numtide/flake-utils";
 
-    nur_dguibert.url     = "github:dguibert/nur-packages/master";
+    nur_dguibert.url     = "github:dguibert/nur-packages/pu";
     nur_dguibert.inputs.nixpkgs.follows = "nixpkgs";
     nur_dguibert.inputs.nix.follows = "nix";
     nur_dguibert.inputs.flake-utils.follows = "flake-utils";
@@ -18,8 +19,8 @@
     nixpack.inputs.spack.follows = "spack";
     nixpack.inputs.nixpkgs.follows = "nixpkgs";
 
-    spack = { url = "git+https://castle.frec.bull.fr:24443/bguibertd/spack.git?ref=develop"; flake=false; };
-    #spack = { url = "git+https://gitlab.bench.local:24443/bguibertd/spack.git?ref=develop"; flake=false; };
+    #spack = { url = "git+https://castle.frec.bull.fr:24443/bguibertd/spack.git?ref=develop"; flake=false; };
+    spack = { url = "git+https://gitlab.bench.local:24443/bguibertd/spack.git?ref=develop"; flake=false; };
     #spack = { url = "git+file:///home_nfs/bguibertd/software-cepp-spack/spack?ref=develop"; flake=false; };
     #spack = { url = "git+file:///home_nfs/bguibertd/software-cepp-spack/spack"; flake=false; };
   };
