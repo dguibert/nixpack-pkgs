15e9c3239334f096f62013d79c4e9dae6041369b
David Guibert <david.guibert@gmail.com>
2023-08-25 13:35:40 +0200

    ddfacet: play with deps (pip don't find numpy)

--- confs/ddfacet.nix
+++ confs/ddfacet.nix
@@ -3,17 +3,21 @@ pack._merge (self:
     with self; {
       label = "ddfacet_" + pack.label;
 
+      package.py-ddfacet.version = "git.54169950a9ba5de038a84ed8b5a65e1f1da3f918=mpi";
       # py-ddfacet dependency py-astropy: package py-astropy@5.1~all build_system=python_pip does not match dependency constraints {"version":"3.0:4.1"}
       package.py-astropy.version = "4";
       # py-astropy dependency cfitsio: package cfitsio@4.2.0+bzip2+shared build_system=autotools does not match dependency constraints {"version":":3"}
       package.cfitsio.version = "3";
       # py-numpy dependency py-cython: package py-cython@3.0.0 build_system=python_pip does not match dependency constraints {"version":"0.29.34:2"}
       package.py-numpy.version = "1.19.5";
-      package.python.version = "3.9";
+      package.python.version = "3.8"; # ddfacet requires <3.9,>=3.0
+      package.py-ipython.version = "8.11.0";
       package.py-numba.version = "0.56.4";
       package.py-llvmlite.version = "0.39";
 
       # as cython is a build dep only will be taken from default pack
+      package.py-mpi4py.depends.py-pip = self.pack.pkgs.py-pip;
+      package.py-mpi4py.depends.py-cython = self.pack.pkgs.py-cython;
       package.py-numpy.depends.py-cython = self.pack.pkgs.py-cython;
       package.py-cython.version = "0.29.34";
 
@@ -53,7 +57,7 @@ pack._merge (self:
       # casacore dependency wcslib: package wcslib@7.3~cfitsio~x build_system=autotools does not match dependency constraints {"variants":{"cfitsio":true},"version":"4.20:"}
       package.wcslib.variants.cfitsio = true;
 
-      package.py-regions.version = "0.5";
+      package.py-regions.version = "0.7";
       package.py-pywavelets.version = "1.1.1";
       package.py-pywavelets.depends.py-setuptools = self.pack.pkgs.py-setuptools;
 
@@ -70,12 +74,16 @@ pack._merge (self:
       package.py-pygments.depends.py-setuptools = self.pack.pkgs.py-setuptools.withPrefs {version = "62.3.2";};
 
       repoPatch = {
+        py-regions.build.setup = ''
+          os.environ['HOME'] = os.environ['TMPDIR']
+        '';
         py-ddfacet = spec: old: {
           depends =
             old.depends
             // {
               py-cython.version = "0.29.34";
               py-pyfftw.version = "0.13.1";
+              py-regions = null; #fails to build
             };
         };
       };
@@ -84,6 +92,78 @@ pack._merge (self:
         compiler
         #py-numpy
         #py-cython
-        py-ddfacet
+        #py-ddfacet
+        py-mpi4py
+        py-tqdm
+        # ./run spack info py-ddfacet -> Build Dependencies
+        py-astlib
+        py-deap
+        py-numpy
+        py-ptyprocess
+        py-pywavelets
+        py-six
+        py-astropy
+        py-ephem
+        py-pandas
+        py-pybind11
+        /*
+        py-regions
+        */
+        py-tables
+        py-codex-africanus
+        py-ipdb
+        py-pip
+        py-pycpuinfo
+        py-ruamel-yaml
+        py-wheel
+        py-configparser
+        py-matplotlib
+        py-polygon3
+        py-pyfftw
+        py-scipy
+        python
+        py-cython
+        py-nose
+        py-prettytable
+        py-pylru
+        py-setuptools
+        py-dask
+        py-numexpr
+        py-psutil
+        py-python-casacore
+        py-sharedarray
+        # ./run spack info py-ddfacet -> Link Dependencies
+        py-astlib
+        py-dask
+        py-nose
+        py-prettytable
+        py-pyfftw
+        py-ruamel-yaml
+        py-astropy
+        py-deap
+        py-numexpr
+        py-psutil
+        py-pylru
+        py-scipy
+        py-codex-africanus
+        py-ephem
+        py-numpy
+        py-ptyprocess
+        py-python-casacore
+        py-sharedarray
+        py-configparser
+        py-ipdb
+        py-pandas
+        py-pybind11
+        py-pywavelets
+        py-six
+        py-cython
+        py-matplotlib
+        py-polygon3
+        py-pycpuinfo
+        /*
+        py-regions
+        */
+        py-tables
       ];
     })
