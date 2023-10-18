ee4ca3d3239eca0b804edc0c13c825046e336848
David Guibert <david.guibert@gmail.com>
2023-10-18 21:48:03 +0200

    format overlays/default.nix

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,30 +4,29 @@
   inputs,
   ...
 }:
-with lib;
-let
-  overlays = mapAttrs'
-  (name: type: {
-    name = removeSuffix ".nix" name;
-    value = let
-      file = ./. + "/${name}";
-    in (final: prev:
-      import file final (
-        prev
-        // {
-          inherit inputs;
-        }
-      ));
-  })
-  (filterAttrs
-    (
-      name: type:
-        (type == "directory" && builtins.pathExists "${toString ./.}/${name}/default.nix")
-        || (type == "regular" && hasSuffix ".nix" name && ! (name == "default.nix") && ! (name == "overlays.nix"))
-        || (type == "symlink" && hasSuffix ".nix" name && ! (name == "default.nix") && ! (name == "overlays.nix"))
-    )
-    (builtins.readDir ./.));
-
+with lib; let
+  overlays =
+    mapAttrs'
+    (name: type: {
+      name = removeSuffix ".nix" name;
+      value = let
+        file = ./. + "/${name}";
+      in (final: prev:
+        import file final (
+          prev
+          // {
+            inherit inputs;
+          }
+        ));
+    })
+    (filterAttrs
+      (
+        name: type:
+          (type == "directory" && builtins.pathExists "${toString ./.}/${name}/default.nix")
+          || (type == "regular" && hasSuffix ".nix" name && ! (name == "default.nix") && ! (name == "overlays.nix"))
+          || (type == "symlink" && hasSuffix ".nix" name && ! (name == "default.nix") && ! (name == "overlays.nix"))
+      )
+      (builtins.readDir ./.));
 in {
   config.flake.overlays.default = overlays;
 }
