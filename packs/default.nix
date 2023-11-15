714250e5918a7e0de48735e0c67229669ff618e2
David Guibert <david.guibert@gmail.com>
2023-11-15 21:59:54 +0100

    libszip: depend on core compiler

--- packs/default.nix
+++ packs/default.nix
@@ -164,6 +164,7 @@ default_pack._merge (self:
         patchelf.version = "0.17";
 
         # no need to be recompiled for each compiler
+        libszip.depends.compiler = packs.default.pack.pkgs.compiler;
         libpng.depends.compiler = packs.default.pack.pkgs.compiler;
         gmake.depends.compiler = packs.default.pack.pkgs.compiler;
         unzip.depends.compiler = packs.default.pack.pkgs.compiler;
